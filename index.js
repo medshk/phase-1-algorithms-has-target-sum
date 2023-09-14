cfc421ebd0dd6ed9e33e96cef691f3a60f9c6833
med <mohammed.bennaceur98@gmail.com>
2023-09-14 14:04:05 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let hasTargetSum = false;
+  array.find((element, index) => {
+    for (let i = index + 1; i < array.length; i++) {
+      if (element + array[i] === target) {
+        hasTargetSum = true;
+        break;
+      }
+      continue;
+    }
+    return hasTargetSum;
+  });
+  return hasTargetSum;
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  Time complexity: O(n²)
+  Space Complexity: 0(1)
+
 */
 
 /* 
   Add your pseudocode here
+  given array of number of length L
+  target : number
+
+  initialize Algotithme
+  hasTargetSum = false
+
+  for i = 0 to L
+   for j = i to L
+      if(array[j] + array[i] = target) : 
+      hasTargetSum = true 
+      break for
+      else : 
+      continue for
+  end for
+  if (hasTargetSum = trye): 
+  break:
+ end for
+ 
+ return hasTagetSum
+
 */
 
 /*
   Add written explanation of your solution here
+
+  first iterate through the array for each element 
+  check if its addition with the next elemnt equals the target
+  when moving to the next element start the second loop from the same index
+  as the first loop for optimization purposes
 */
 
 // You can run `node index.js` to view these console logs
